dbea95e31f0b6b90d47bef99aa33f8bc401e421d
ebwu95 <38186996+ebwu95@users.noreply.github.com>
2022-11-25 21:03:30 -0500

    added stuff

---
+++ my-app/src/pages/lobby.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Lobby(props) {
+    return (
+        <div> 
+            <div className="code-information"> 
+                <h3> Code: </h3>
+                <h2> AAAA </h2>
+            </div>
+            <h1> Settings </h1>
+        </div>
+    )
+}
+
+export default Lobby;
