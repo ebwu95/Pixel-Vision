4d7a80e070b97a6f047db52794a957f32cdf7b04
RaymondMei <raymondmei.rm@gmail.com>
2022-11-25 21:01:52 -0500

    start styling homepage

--- my-app/src/components/Grid5x5.js
+++ my-app/src/components/Grid5x5.js
@@ -1,20 +1,43 @@
 import React from "react"
+// function Grid5x5() {
+//   const cells = []
+//   for (let i = 0; i < 25; i++) {
+//     cells.push(<div style={{backgroundColor: "white"}} className="grid-item"> </div>)
+//   }
+//   cells[6] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
+//   cells[8] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
+//   cells[6] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
+//   cells[16] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
+//   cells[17] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
+//   cells[18] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>  
+//   return (
+//     <div className="grid-container">
+//         {cells}
+//       </div>
+//     )
+// }
+
+
 function Grid5x5() {
-  const cells = []
-  for (let i = 0; i < 25; i++) {
-    cells.push(<div style={{backgroundColor: "white"}} className="grid-item"> </div>)
-  }
-  cells[6] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
-  cells[8] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
-  cells[6] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
-  cells[16] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
-  cells[17] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>
-  cells[18] = <div style={{backgroundColor: "black"}} className="grid-item"> </div>  
   return (
-    <div className="grid-container">
-        {cells}
+    <div className="border border-1 m-5 w-80 h-100">
+      <div className="row">
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
       </div>
-    )
+      <div className="row w-10">
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+      </div>
+      <div className="row w-10">
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+        <span className="border border-3 border-dark col vw-9.5 vh-9.5"></span>
+      </div>
+    </div>
+  )
 }
 
 export default Grid5x5;
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -3,19 +3,28 @@ import Grid5x5 from "./Grid5x5"
 
 function Home() {
     return (
-        <div> 
-            <header> Pixel Vision </header>
-            <div className = "home-container"> 
-                <Grid5x5 />
-                <div className = "home-buttons-container">
-                    <input className = "name-input" type = "text" placeholder="NAME" />
-                    <div className = "code-input-container">
-                        <input className = "code-input" type = "text" placeholder="CODE" />
-                        <input className = "join-button" type="button" value="JOIN"/>
+        <div className="h-100 position-absolute container-fluid bg-primary d-flex flex-column">
+            <header className="text-center h1"> Pixel Vision </header>
+            <div className="row flex-fill">
+                <div className="col-sm-8 bg-dark">
+                    <Grid5x5 />
+                </div>
+
+                <div className="col-sm-4 bg-danger d-flex flex-column justify-content-center">
+                    <div className="border border-1 mx-5">
+                        <div className="input-group p-2">
+                            <span class="input-group-text px-3">NAME</span>
+                            <input className="form-control" type="text" />
+                        </div>
+                        <div className="input-group p-2">
+                            <input className="form-control" type="text" placeholder="CODE" />
+                            <button className="btn btn-success" type="button">JOIN</button>
+                        </div>
+                        <input className="create-room-button btn btn-outline-success" type="button" value="CREATE ROOM" />
                     </div>
-                    <input className = "create-room-button" type="button" value="CREATE ROOM"/>
                 </div>
             </div>
+
         </div>)
 }
 
