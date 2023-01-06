78f5184335d01e5e6c7aa375cd03ffcb4e4d2b14
RaymondMei <raymondmei.rm@gmail.com>
2023-01-06 00:41:27 -0500

    change playerlist to work with props

--- client/my-app/components/PlayerList.js
+++ client/my-app/components/PlayerList.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PlayerListTile from './PlayerListTile.js'
 
-function PlayerList(props) {
+function PlayerList({ players }) {
     return (
         <div className="h-100 pt-1 d-flex flex-column justify-content-start">
-            <PlayerListTile username="Eric" color="Blue" />
+            {/* <PlayerListTile username="Eric" color="Blue" />
             <PlayerListTile username="Raymond" color="Red" />
             <PlayerListTile username="Evan" color="Green" />
             <PlayerListTile username="Kevin" color="Yellow" />
             <PlayerListTile username="Raymond" color="Red" />
             <PlayerListTile username="Raymond" color="Red" />
             <PlayerListTile username="Raymond" color="Red" />
-            <PlayerListTile username="Raymond" color="Red" />
+            <PlayerListTile username="Raymond" color="Red" /> */}
+            {players.map((player) => <PlayerListTile username={player} />)}
 
         </div>
     )
--- client/my-app/components/PlayerListTile.js
+++ client/my-app/components/PlayerListTile.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function PlayerListTile({ username, color, }) {
+function PlayerListTile({ username }) {
     return (
-        <div className="p-4 text-center border-bottom">{`${username} ${color}`}</div>
+        <div className="p-4 text-center border-bottom">{`${username}`}</div>
     )
 }
 
