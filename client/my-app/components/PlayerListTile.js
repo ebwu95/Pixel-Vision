04d21980bc6ad0d3b016d8b72c824a210565f133
RaymondMei <raymondmei.rm@gmail.com>
2023-01-06 22:10:31 -0500

    change playerlist to work with props

--- client/my-app/components/PlayerListTile.js
+++ client/my-app/components/PlayerListTile.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PlayerListTile({ username, color, }) {
     return (
-        <div className="p-4 text-center border-bottom">{`${username} ${color}`}</div>
+        <div className="p-4 text-center border-bottom">{`${username}`}</div>
     )
 }
 
