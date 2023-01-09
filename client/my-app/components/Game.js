c56a3151f682f3ab290066c41f62faf48081bf01
kevinasian122 <yanchenkai@gmail.com>
2023-01-08 22:16:53 -0500

    Made function that generates random grid, moved boxes state to Game.js component

--- client/my-app/components/Game.js
+++ client/my-app/components/Game.js
@@ -8,6 +8,7 @@ function Game({name, players, socket}) {
     console.log(colour);
     const [timer, setTimer] = useState(30);
     const [score, setScore] = useState(new Array(players.length).fill(0));
+   
     useEffect(() => {
         if (timer == 0) {
             return;
@@ -17,6 +18,34 @@ function Game({name, players, socket}) {
         }, 1000)
         return () => clearInterval(interval);
     }, [timer])
+
+    //function to generate random grid
+    const generateGrid = () => {
+        const height = Math.floor(Math.random() * 5) + 1
+        const width = Math.floor(Math.random() * 5) + 1
+        const colours = ['red', 'green', 'yellow', 'blue', 'black']
+        setBoxes(() => {
+            const rows = []
+            for (let i = 0; i < height; i++) {
+                const column = []
+                for (let j = 0; j < width; j++){
+                    const ind = Math.floor(Math.random() * 5)
+                    column.push(colours[ind])
+                }
+                rows.push(column)
+            }
+            return rows
+        })
+    }
+    // 2d State with colour of each box
+    const [boxes, setBoxes] = useState(() => {
+        const rows = []
+        for (let i = 0; i < 3; i++) {
+        rows.push(new Array(3).fill('white'))
+        }
+        return rows
+    })
+    
 
     return (
     <>
@@ -29,9 +58,10 @@ function Game({name, players, socket}) {
                 </div>
         </div>
         <div className="h-100 d-flex flex-row align-items-center justify-content-center">
+            <button onClick={() => generateGrid()}>TEMP BUTTON</button>
             <div className="bg-light bg-light mx-3 rounded shadow col-sm-6 d-flex flex-column justify-content-center align-items-center"
                 style={{height: "75vh", width: "75vh"}}>
-                <GameGrid height={10} width={10} colour={colour} />
+                <GameGrid height={boxes.length} width={boxes[0].length} colour={colour} boxes={boxes} setBoxes={setBoxes} />
             </div>
             <div className="colours col-5 d-flex flex-row justify-content-center gap-4 position-absolute top-0 p-5">
                 <div className= "rounded-circle shadow-lg border border-3 border-dark" style={{'aspect-ratio': '1 / 1', height:'100%', minHeight:'10px', 'background-color':'red', cursor:'pointer'}} onClick = {() => {setColour('red')}}></div>
