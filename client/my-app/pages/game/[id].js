05b56d2b875f0d55739d8232eadea7b29ca91b21
ebwu95 <38186996+ebwu95@users.noreply.github.com>
2023-01-13 15:45:55 -0500

    Update [id].js

--- client/my-app/pages/game/[id].js
+++ client/my-app/pages/game/[id].js
@@ -18,40 +18,19 @@ export default function Lobby({ name, socket }) {
             setRound(data.round);
             setBoxes(data.boxes);
             setScore(data.scores)
-            console.log(data);
             setInGame(true);
         }) 
         socket.on('start_round_1', (data) => {
             setRound(data.round);
             setBoxes(data.boxes);
-            console.log(round);
         })
     }, [socket]);
     const isCreator = socket.id == router.query.id
     
     if (inGame) {
-        console.log(round);
         return <Game score={score} boxes={boxes} setBoxes={setBoxes} round={round} timeLimit={5 * round} isCreator={isCreator} playerID={players.findIndex((player) => player === name)} socket={socket} players={players} /> 
     }
     else {
         return <LobbyLayout socket={socket} id={router.query.id} isCreator={isCreator} players={players} />
     }
 }
-
-// export function getAllLobbies(){
-//     return [{params: {id: 'ABCD'}}, {params: {id: 'EFGH'}}, {params: {id: 'IJKL'}}];
-// }n
-
-// export async function getStaticPaths() {
-//     const paths = getAl  lLobbies();
-//     return {paths, fallback: false,};
-// }
-
-// export async function getStaticProps({ params }) {
-//     console.log(params.id);
-//     return {
-//         props: {
-//             id: params.id,
-//         },
-//     }
-// }
